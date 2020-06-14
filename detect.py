3a4c92ebfe5c1ac8258af19c45145f95979b02ae
torjeikenes <torje.n.eikenes@gmail.com>
2020-06-14 19:58:29 +0200

    Cells ready for ocr

--- detect.py
+++ detect.py
@@ -11,6 +11,7 @@ Detect a sudoku grid
 import cv2
 import imutils
 from imutils import perspective
+from imutils import contours
 import argparse
 import logging
 import numpy as np
@@ -28,20 +29,24 @@ def binaryImage(gray):
     gausTresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                       cv2.THRESH_BINARY_INV,11,2)
 
+    cv2.imshow("CV",gausTresh)
+    cv2.waitKey(0)
     # Dilate and erode to make sure the grid contour connects
     dilated = cv2.dilate(gausTresh,None,iterations=1)
     eroded = cv2.erode(dilated,None,iterations=1)
     
-
+    cv2.imshow("CV",eroded)
+    cv2.waitKey(0)
+    
     return eroded
 
 def getGridCorners(binaryImg):
     '''Return conrers'''
     #contours = cv2.findContours(binaryImg,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    contours = cv2.findContours(binaryImg,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-    contours = imutils.grab_contours(contours)
+    cnts = cv2.findContours(binaryImg,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+    cnts = imutils.grab_contours(cnts)
 
-    grid = max(contours,key=cv2.contourArea)
+    grid = max(cnts,key=cv2.contourArea)
 
     # Find rectangle to get corners
     epsilon = 0.1*cv2.arcLength(grid,True)
@@ -59,8 +64,8 @@ def getGridCorners(binaryImg):
     return pts
 
 def filterOutNumber(warp):
-    contours = cv2.findContours(warp,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(contours)
+    cnts = cv2.findContours(warp,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
 
     mask = warp.copy()
 
@@ -68,43 +73,81 @@ def filterOutNumber(warp):
     cellVol = warp.shape[0]//9*warp.shape[1]//9
     
     # Filtering out numbers
-    for c in contours:
+    for c in cnts:
         if cv2.contourArea(c)< cellVol:
             cv2.drawContours(mask,[c],-1,(0,0,0),-1)
 
-
-    cv2.imshow("CV-warp",mask)
-    cv2.waitKey(0)
-    
     # Repair grid lines
     verticalKernel = cv2.getStructuringElement(cv2.MORPH_RECT,(1,5))
     mask = cv2.morphologyEx(mask,cv2.MORPH_CLOSE,verticalKernel,iterations=5)
     horizontalKernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5,1))
     mask = cv2.morphologyEx(mask,cv2.MORPH_CLOSE,horizontalKernel,iterations=5)
 
-    cv2.imshow("CV-warp",mask)
-    cv2.waitKey(0)
-
-
     inverted = cv2.bitwise_not(mask)
+
+    return inverted
+    
+    
+def getMatrix(img,mask):
+    '''Return matrix with the sudoku numbers'''
+    cnts = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    # Sort the contours from top to bottom
+    verticalSort,_ = contours.sort_contours(cnts,method="top-to-bottom")
+
+    numberGrid = []
+    row = []
+    # Sorts the cells on the row from left to right
+    for i,c in enumerate(verticalSort,1):
+        row.append(c)
+        if i%9 == 0:
+            horizontalSort,_ = contours.sort_contours(row,method="left-to-right")
+            numberGrid.append(horizontalSort)
+            row = []
+    
+    blur = cv2.GaussianBlur(img,(7,7),0)
+
+
+    # Empty sudoku grid to be filled 
+    sudokuMatrix = np.zeros((9,9),dtype=np.uint8)
+    out = img.copy()
+    # Iterates trough all the cells from top to bottom left to right
+    for i,r in enumerate(numberGrid):
+        for j,c in enumerate(r):
+            logging.debug("row: "+ str(i) + " col: "+ str(j))
+            cell = np.zeros(img.shape,dtype=np.uint8)
+            # Single out one cell
+            cell = cv2.drawContours(cell,[c],-1,(255,255,255),-1)
+            cellImg = cv2.bitwise_and(blur,cell)
+            number = getNumber(cellImg)
+
+def getNumber(cellImg):
+    # Tresholding before ocr
+    clean = cv2.adaptiveThreshold(cellImg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                      cv2.THRESH_BINARY,5,2)
     
 
-
-
+    #dilated = cv2.dilate(clean,None,iterations=1)
+    #eroded = cv2.erode(clean,None,iterations=2)
+    #cv2.imshow("CV",dilated)
+    cv2.imshow("CV2",clean)
+    cv2.waitKey(0)
 
 
 
 def main(image):
     #preprocessing
-    resize = imutils.resize(image,height=500)
+    resize = imutils.resize(image,height=700)
     gray = cv2.cvtColor(resize,cv2.COLOR_BGR2GRAY)
 
     binary = binaryImage(gray)
     pts = getGridCorners(binary)
-    warp = perspective.four_point_transform(binary,pts)
-    filterOutNumber(warp)
-    cv2.imshow("CV-warp",warp)
-    cv2.waitKey(0)
+    binWarp = perspective.four_point_transform(binary,pts)
+    cellMask = filterOutNumber(binWarp)
+
+    grayWarp = perspective.four_point_transform(gray,pts)
+    getMatrix(grayWarp,cellMask)
 
 
 
